175411dd1d125e98a332094f1b72d9d429dfd74b
rachitmehta96 <mehta.rac@northeastern.edu>
2022-04-03 10:59:05 -0400

    added dummy data to exercise recycler view and added images to drawable directory

--- app/src/main/java/edu/neu/madcourse/trexercize/ui/fragments/exercise/ExerciseFragment.kt
+++ app/src/main/java/edu/neu/madcourse/trexercize/ui/fragments/exercise/ExerciseFragment.kt
@@ -5,56 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import edu.neu.madcourse.trexercize.R
+import edu.neu.madcourse.trexercize.ui.fragments.profile.ProfileCard
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ExerciseFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ExerciseFragment : Fragment(R.layout.fragment_exercise) {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private val exerciseList: MutableList<ExerciseCard> = ArrayList()
+    private var recyclerView: RecyclerView? = null
+    private var exerciseAdapter: ExerciseAdapter? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView = view.findViewById(R.id.exercise_recycler_view)
+
+        setUpResources()
+        setUpData()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exercise, container, false)
-    }
+    private fun setUpData(){
+        exerciseList.add(ExerciseCard("Chest", R.drawable.chest))
+        exerciseList.add(ExerciseCard("Arms", R.drawable.arms))
+        exerciseList.add(ExerciseCard("Back", R.drawable.back))
+        exerciseList.add(ExerciseCard("Abs", R.drawable.abs))
+        exerciseList.add(ExerciseCard("Legs", R.drawable.legs))
+        exerciseList.add(ExerciseCard("Full Body", R.drawable.full_body))
+        exerciseList.add(ExerciseCard("HIIT", R.drawable.dumbell))
+        exerciseList.add(ExerciseCard("Cardio", R.drawable.dumbell))
+        exerciseAdapter?.notifyDataSetChanged()
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ExerciseFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ExerciseFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    }
+    private fun setUpResources(){
+        exerciseAdapter = this.context?.let { ExerciseAdapter(exerciseList, it) }
+        recyclerView!!.adapter = exerciseAdapter
+        recyclerView!!.layoutManager = GridLayoutManager(context, 2)
     }
 }
