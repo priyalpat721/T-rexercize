589e1229609ff011cddd0749b75b410a6f698c99
rachitmehta96 <mehta.rac@northeastern.edu>
2022-04-03 09:59:20 -0400

    initialized exercise card

--- app/src/main/java/edu/neu/madcourse/trexercize/ui/fragments/exercise/ExerciseCard.kt
+++ app/src/main/java/edu/neu/madcourse/trexercize/ui/fragments/exercise/ExerciseCard.kt
@@ -1,4 +1,12 @@
 package edu.neu.madcourse.trexercize.ui.fragments.exercise
 
 class ExerciseCard {
+    var image: Int? = null
+    var title: String? = null
+
+    constructor()
+    constructor(title: String?, image:Int?){
+        this.image = image
+        this.title = title
+    }
 }
