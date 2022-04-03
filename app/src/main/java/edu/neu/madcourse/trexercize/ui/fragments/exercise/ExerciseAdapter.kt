ac84d9119d7f9adec8d2bb9f3d586b69022de1e5
rachitmehta96 <mehta.rac@northeastern.edu>
2022-04-03 10:09:27 -0400

    initialized Exercise Adapter and ViewHolder

--- app/src/main/java/edu/neu/madcourse/trexercize/ui/fragments/exercise/ExerciseAdapter.kt
+++ app/src/main/java/edu/neu/madcourse/trexercize/ui/fragments/exercise/ExerciseAdapter.kt
@@ -1,4 +1,28 @@
 package edu.neu.madcourse.trexercize.ui.fragments.exercise
 
-class ExerciseAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import edu.neu.madcourse.trexercize.R
+
+class ExerciseAdapter(
+    private var exerciseList: MutableList<ExerciseCard>,
+    private var context: Context
+) : RecyclerView.Adapter<ExerciseViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExerciseViewHolder {
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.exercise_card_layout, parent, false)
+        return ExerciseViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ExerciseViewHolder, position: Int) {
+        holder.title.text = exerciseList[position].title.toString()
+        exerciseList[position].image?.let { holder.image.setImageResource(it) }
+    }
+
+    override fun getItemCount(): Int {
+        return exerciseList.size
+    }
+
 }
--- app/src/main/java/edu/neu/madcourse/trexercize/ui/fragments/exercise/ExerciseViewHolder.kt
+++ app/src/main/java/edu/neu/madcourse/trexercize/ui/fragments/exercise/ExerciseViewHolder.kt
@@ -1,4 +1,12 @@
 package edu.neu.madcourse.trexercize.ui.fragments.exercise
 
-class ExerciseViewHolder {
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import edu.neu.madcourse.trexercize.R
+
+class ExerciseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    var title: TextView = itemView.findViewById(R.id.body_part_title)
+    var image: ImageView = itemView.findViewById(R.id.body_part)
 }
