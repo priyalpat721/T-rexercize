3c9c45c15aa2070223e01baef070cbfce594727f
priyalpat721 <priyalpat721@gmail.com>
2022-04-05 02:56:13 -0400

    Edit page has fields prepopulated. Button set up for picture

--- app/src/main/java/edu/neu/madcourse/trexercize/ui/fragments/profile/ProfileFragment.kt
+++ app/src/main/java/edu/neu/madcourse/trexercize/ui/fragments/profile/ProfileFragment.kt
@@ -2,7 +2,6 @@ package edu.neu.madcourse.trexercize.ui.fragments.profile
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Button
