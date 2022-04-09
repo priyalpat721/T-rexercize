f3b4755eaffd1e8f6cc802eec26f1e21abc92c99
priyalpat721 <priyalpat721@gmail.com>
2022-04-09 13:48:13 -0400

    added carousel to signin/signup

--- app/src/main/java/edu/neu/madcourse/trexercize/ui/fragments/userAuth/SignInActivity.kt
+++ app/src/main/java/edu/neu/madcourse/trexercize/ui/fragments/userAuth/SignInActivity.kt
@@ -4,18 +4,25 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import edu.neu.madcourse.trexercize.R
 import edu.neu.madcourse.trexercize.ui.HomeActivity
+import edu.neu.madcourse.trexercize.ui.helper.CarouselData
 
 class SignInActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
@@ -25,27 +32,35 @@ class SignInActivity : AppCompatActivity() {
     private lateinit var progressBar: ProgressBar
     private var db = Firebase.database.reference
     private lateinit var signUp: Button
+    private lateinit var viewPager: ViewPager2
+    private var carouselList = ArrayList<CarouselItem>()
+    var carouselHandler : Handler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
-        val token : String? = intent.getStringExtra("token")
-        Log.d("Sign In", "token : $token")
+        populateList()
 
+        val title = findViewById<TextView>(R.id.sign_in_sign)
+        title.visibility = View.VISIBLE
         signUp = findViewById(R.id.to_sign_up_page)
         auth = Firebase.auth
         warning = findViewById(R.id.warning_sign)
         usernameOrEmail = findViewById(R.id.sign_in_email)
         signInButton = findViewById(R.id.sign_in_button)
+        signInButton.visibility = View.VISIBLE
         progressBar = findViewById(R.id.progress_bar_sign_in)
-        progressBar.visibility = View.INVISIBLE
+        viewPager = findViewById(R.id.carousel)
+        signUp.visibility = View.VISIBLE
+        usernameOrEmail.visibility = View.VISIBLE
 
         signInButton.setOnClickListener {
             progressBar.visibility = View.VISIBLE
             val emailOrUsername = usernameOrEmail.text.toString()
-            signIn(emailOrUsername, "password")
+            signIn(emailOrUsername)
             // get token for the user
+            progressBar.visibility = View.INVISIBLE
         }
 
         signUp.setOnClickListener {
@@ -54,26 +69,48 @@ class SignInActivity : AppCompatActivity() {
             startActivity(intent)
 
         }
+        val carouselData = CarouselData()
+        carouselData.getCarouselData(carouselList, viewPager, run, carouselHandler, this)
 
+    }
+    private val run = Runnable {
+        viewPager.currentItem = viewPager.currentItem + 1
+    }
+
+    override fun onResume() {
+        super.onResume()
+        carouselHandler.postDelayed(run, 1500)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        carouselHandler.removeCallbacks(run)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewPager.unregisterOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+
+        })
     }
 
     @SuppressLint("SetTextI18n")
-    private fun signIn(userNameOrEmail: String, password: String) {
+    private fun signIn(userNameOrEmail: String) {
 
         warning.text = ""
 
-        if(userNameOrEmail.equals("")){
+        if (userNameOrEmail == "") {
+            warning.visibility = View.VISIBLE
             progressBar.visibility = View.VISIBLE
             Thread.sleep(1000)
-            progressBar.visibility = View.GONE
+            progressBar.visibility = View.INVISIBLE
             warning.text = "You don't have any text in the Email field. Please try again."
             return
         }
 
-        auth.signInWithEmailAndPassword(userNameOrEmail, password)
+        auth.signInWithEmailAndPassword(userNameOrEmail, "password")
             .addOnCompleteListener(this@SignInActivity) { task ->
                 if (task.isSuccessful) {
-
                     val userRef = db.child("users")
                     userRef.get().addOnSuccessListener { document ->
                         if (document != null) {
@@ -86,19 +123,30 @@ class SignInActivity : AppCompatActivity() {
                         .addOnFailureListener {
                         }
                 } else {
+                    warning.visibility = View.VISIBLE
                     Thread.sleep(1000)
-                    progressBar.visibility = View.GONE
+                    progressBar.visibility = View.INVISIBLE
                     warning.text = "Email is invalid! Check email or sign up by clicking here."
                     warning.setOnClickListener {
                         val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
                         finish()
-                        progressBar.visibility = View.GONE
+                        progressBar.visibility = View.INVISIBLE
                         startActivity(intent)
 
                     }
 
                 }
             }
+    }
+
+    private fun populateList() {
+        carouselList.add(CarouselItem("https://firebasestorage.googleapis.com/v0/b/t-rexercize.appspot.com/o/bubbledino.png?alt=media&token=8896212f-cc4e-4677-b7e6-45575e3c3a0f"))
+        carouselList.add(CarouselItem("https://firebasestorage.googleapis.com/v0/b/t-rexercize.appspot.com/o/exercisedino.png?alt=media&token=9ef0f90d-80fa-4e5d-9747-856baf0024c8"))
+        carouselList.add(CarouselItem("https://firebasestorage.googleapis.com/v0/b/t-rexercize.appspot.com/o/frustratedino.png?alt=media&token=59cbbe30-0715-46e8-910c-9958b14c2a30"))
+        carouselList.add(CarouselItem("https://firebasestorage.googleapis.com/v0/b/t-rexercize.appspot.com/o/happydino.png?alt=media&token=112f3cba-0272-4a0d-af18-263b3b65b9fb"))
+        carouselList.add(CarouselItem("https://firebasestorage.googleapis.com/v0/b/t-rexercize.appspot.com/o/motivatedino.png?alt=media&token=ccaf03c3-c640-4a7e-8ca1-58cd617313d3"))
+        carouselList.add(CarouselItem("https://firebasestorage.googleapis.com/v0/b/t-rexercize.appspot.com/o/saddino.png?alt=media&token=a2944664-2f88-4095-81e3-678c8bb0e316"))
+        carouselList.add(CarouselItem("https://firebasestorage.googleapis.com/v0/b/t-rexercize.appspot.com/o/sleepdino2.png?alt=media&token=44d3ce0d-cfac-4f1c-8f3b-484aaba37343"))
 
     }
 }
--- app/src/main/java/edu/neu/madcourse/trexercize/ui/fragments/userAuth/SignUpActivity.kt
+++ app/src/main/java/edu/neu/madcourse/trexercize/ui/fragments/userAuth/SignUpActivity.kt
@@ -4,9 +4,16 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.View
 import android.widget.*
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
 import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -14,6 +21,7 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import edu.neu.madcourse.trexercize.R
 import edu.neu.madcourse.trexercize.ui.HomeActivity
+import edu.neu.madcourse.trexercize.ui.helper.CarouselData
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var name: EditText
@@ -24,21 +32,38 @@ class SignUpActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var warningSignUp: TextView
     private var db = Firebase.database.reference
+    private lateinit var viewPager: ViewPager2
+    private var carouselList = ArrayList<CarouselItem>()
+    var carouselHandler : Handler = Handler(Looper.getMainLooper())
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        val title = findViewById<TextView>(R.id.welcome)
+        title.visibility = View.VISIBLE
         warningSignUp = findViewById(R.id.warning_sign_up)
         warningSignUp.text = ""
         warningSignUp = findViewById(R.id.warning_sign_up)
         warningSignUp.text = ""
         name = findViewById(R.id.name)
+        name.visibility = View.VISIBLE
         email = findViewById(R.id.email)
+        email.visibility = View.VISIBLE
         signUp = findViewById(R.id.sign_up)
+        signUp.visibility = View.VISIBLE
         switchToLogIn = findViewById(R.id.to_login_page)
+        switchToLogIn.visibility = View.VISIBLE
         progressBar = findViewById(R.id.progress_bar)
-        progressBar.visibility = View.GONE
+        viewPager = findViewById(R.id.carousel_up)
+
+        populateList()
+        val carouselData = CarouselData()
+        carouselData.getCarouselData(carouselList, viewPager, run, carouselHandler, this)
+
         switchToLogIn.setOnClickListener {
+            carouselList.clear()
+            viewPager.adapter?.notifyDataSetChanged()
             val intent = Intent(this@SignUpActivity, SignInActivity::class.java)
             finish()
             startActivity(intent)
@@ -51,7 +76,27 @@ class SignUpActivity : AppCompatActivity() {
             signup(emailAddress, nameText)
         }
         auth = Firebase.auth
+    }
 
+    private val run = Runnable {
+        viewPager.currentItem = viewPager.currentItem + 1
+    }
+
+    override fun onResume() {
+        super.onResume()
+        carouselHandler.postDelayed(run, 1500)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        carouselHandler.removeCallbacks(run)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewPager.unregisterOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+
+        })
     }
 
     @SuppressLint("SetTextI18n")
@@ -59,8 +104,11 @@ class SignUpActivity : AppCompatActivity() {
         warningSignUp.text = ""
 
         if (emailAddress == "" || nameText == "") {
+            warningSignUp.visibility = View.VISIBLE
+            progressBar.visibility = View.VISIBLE
             Thread.sleep(1000)
-            progressBar.visibility = View.GONE
+            progressBar.visibility = View.INVISIBLE
+            warningSignUp.visibility = View.VISIBLE
             if (nameText == "" && emailAddress == "") {
                 warningSignUp.text = "Name and email fields empty. Please provide above information"
             } else if (emailAddress == "") {
@@ -80,6 +128,8 @@ class SignUpActivity : AppCompatActivity() {
                 progressBar.visibility = View.VISIBLE
 
                 if (task.isSuccessful) {
+                    Thread.sleep(1000)
+                    progressBar.visibility = View.INVISIBLE
                     val calendarDoc = db.child("calendars").push().key.toString()
                     db.child("calendars").child(calendarDoc).setValue(calendar)
                     Log.i("Calendar", calendarDoc)
@@ -119,10 +169,21 @@ class SignUpActivity : AppCompatActivity() {
                     ).show()
                     val intent = Intent(this@SignUpActivity, HomeActivity::class.java)
                     finish()
-                    progressBar.visibility = View.GONE
+                    progressBar.visibility = View.INVISIBLE
                     startActivity(intent)
 
                 }
             }
+    }
+
+    private fun populateList() {
+        carouselList.add(CarouselItem("https://firebasestorage.googleapis.com/v0/b/t-rexercize.appspot.com/o/bubbledino.png?alt=media&token=8896212f-cc4e-4677-b7e6-45575e3c3a0f"))
+        carouselList.add(CarouselItem("https://firebasestorage.googleapis.com/v0/b/t-rexercize.appspot.com/o/exercisedino.png?alt=media&token=9ef0f90d-80fa-4e5d-9747-856baf0024c8"))
+        carouselList.add(CarouselItem("https://firebasestorage.googleapis.com/v0/b/t-rexercize.appspot.com/o/frustratedino.png?alt=media&token=59cbbe30-0715-46e8-910c-9958b14c2a30"))
+        carouselList.add(CarouselItem("https://firebasestorage.googleapis.com/v0/b/t-rexercize.appspot.com/o/happydino.png?alt=media&token=112f3cba-0272-4a0d-af18-263b3b65b9fb"))
+        carouselList.add(CarouselItem("https://firebasestorage.googleapis.com/v0/b/t-rexercize.appspot.com/o/motivatedino.png?alt=media&token=ccaf03c3-c640-4a7e-8ca1-58cd617313d3"))
+        carouselList.add(CarouselItem("https://firebasestorage.googleapis.com/v0/b/t-rexercize.appspot.com/o/saddino.png?alt=media&token=a2944664-2f88-4095-81e3-678c8bb0e316"))
+        carouselList.add(CarouselItem("https://firebasestorage.googleapis.com/v0/b/t-rexercize.appspot.com/o/sleepdino2.png?alt=media&token=44d3ce0d-cfac-4f1c-8f3b-484aaba37343"))
+
     }
 }
