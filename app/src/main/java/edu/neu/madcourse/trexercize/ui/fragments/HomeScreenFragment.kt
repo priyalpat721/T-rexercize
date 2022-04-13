c456471e2f425ed8bd07c9f0b6ac37613326efe7
priyalpat721 <priyalpat721@gmail.com>
2022-04-13 09:24:06 -0400

    made a small change to make nav bar visible

--- app/src/main/java/edu/neu/madcourse/trexercize/ui/fragments/HomeScreenFragment.kt
+++ app/src/main/java/edu/neu/madcourse/trexercize/ui/fragments/HomeScreenFragment.kt
@@ -1,60 +1,40 @@
-package edu.neu.madcourse.trexercize.ui.fragments
+package edu.neu.madcourse.trexercize.ui.fragments.homescreen
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import edu.neu.madcourse.trexercize.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeScreenFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeScreenFragment : Fragment(R.layout.fragment_home_screen) {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private val favoritesList: MutableList<FavoriteExerciseCard> = ArrayList()
+    private var recyclerView: RecyclerView? = null
+    private var homeScreenAdapter: HomeScreenAdapter? = null
+    //TODO: Add DB connection
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView = view.findViewById(R.id.favorites_rec_view)
+        val navBar : BottomNavigationView? = activity?.findViewById(R.id.bottom_navigation_bar)
+        navBar?.visibility = View.VISIBLE
+        setUpResources()
+        setUpData()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_screen, container, false)
+    @SuppressLint("NotifyDataSetChanged")
+    private fun setUpData() {
+        favoritesList.add(FavoriteExerciseCard("Pushup", "Chest", R.drawable.chest))
+        favoritesList.add(FavoriteExerciseCard("Pullup", "Back", R.drawable.back))
+        favoritesList.add(FavoriteExerciseCard("Crunches", "Abs", R.drawable.abs))
+        homeScreenAdapter?.notifyDataSetChanged()
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeScreenFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeScreenFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun setUpResources() {
+        homeScreenAdapter = this.context?.let { HomeScreenAdapter(favoritesList, it) }
+        recyclerView!!.adapter = homeScreenAdapter
+        recyclerView!!.layoutManager = LinearLayoutManager(context)
     }
 }
