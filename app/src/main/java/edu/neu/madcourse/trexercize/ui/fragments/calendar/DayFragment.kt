914ad11163e6d97a57ff12f8488b8ab4e4401cd9
jrwen <35855687+jrwen@users.noreply.github.com>
2022-04-12 21:37:00 -0400

    stickers scroll, but is choppy and need to keep tapping

--- app/src/main/java/edu/neu/madcourse/trexercize/ui/fragments/calendar/DayFragment.kt
+++ app/src/main/java/edu/neu/madcourse/trexercize/ui/fragments/calendar/DayFragment.kt
@@ -3,6 +3,9 @@ package edu.neu.madcourse.trexercize.ui.fragments.calendar
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.view.View.FOCUS_DOWN
+import android.view.View.FOCUS_UP
+import android.widget.HorizontalScrollView
 import android.widget.ImageButton
 import edu.neu.madcourse.trexercize.R
 import android.widget.TextView
@@ -19,10 +22,11 @@ import edu.neu.madcourse.trexercize.ui.fragments.profile.ProfileCard
 class DayFragment : Fragment(R.layout.fragment_day) {
     private val args : DayFragmentArgs by navArgs()
     private val exerciseList: MutableList<ExerciseTextCard> = ArrayList()
-    private var recyclerView: RecyclerView? = null
-    var adapter: ExerciseTextAdapter? = null
-    private var exerciseName: TextView? = null
-    private var exerciseMuscleGroups: TextView? = null
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var stickerScroll: HorizontalScrollView
+    private lateinit var stickerBack: ImageButton
+    private lateinit var stickerForward: ImageButton
+    lateinit var adapter: ExerciseTextAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -46,5 +50,16 @@ class DayFragment : Fragment(R.layout.fragment_day) {
         exerciseList.add(ExerciseTextCard("run", "leg"))
         exerciseList.add(ExerciseTextCard("swim", "heart"))
         adapter?.setDataList(exerciseList)
+
+        stickerScroll = view.findViewById(R.id.stickerScroll)
+        stickerBack = view.findViewById(R.id.stickerBack)
+        stickerForward = view.findViewById(R.id.stickerForward)
+
+        stickerBack.setOnClickListener {
+            stickerScroll.smoothScrollTo(stickerScroll.scrollX - 50, stickerScroll.scrollY)
+        }
+        stickerForward.setOnClickListener {
+            stickerScroll.smoothScrollTo(stickerScroll.scrollX + 50, stickerScroll.scrollY)
+        }
     }
 }
