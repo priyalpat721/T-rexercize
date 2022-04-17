7f0102a2010963e9097114af138d3aa4cc03d109
rachitmehta96 <mehta.rac@northeastern.edu>
2022-04-17 17:40:26 -0400

    Toast added for visual indication

--- app/src/main/java/edu/neu/madcourse/trexercize/ui/fragments/exercise/eachExercise/EachExerciseFragment.kt
+++ app/src/main/java/edu/neu/madcourse/trexercize/ui/fragments/exercise/eachExercise/EachExerciseFragment.kt
@@ -93,6 +93,7 @@ class EachExerciseFragment : Fragment(R.layout.each_exercise_layout) {
                                     Date()
                                 )
                                 db.child("calendars").child(currentCalendarId.toString()).child(date).child("workout").child(exerciseTitle.text.toString()).setValue(currentExercise)
+                                Toast.makeText(context, "Workout added!", Toast.LENGTH_SHORT).show()
                             }
                         }
                     }
