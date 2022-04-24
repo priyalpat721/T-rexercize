5235fe44c41a33b203305d97095a413bb4986736
jrwen <35855687+jrwen@users.noreply.github.com>
2022-04-24 08:42:42 -0400

    alternating colors on homescreen

--- app/src/main/java/edu/neu/madcourse/trexercize/ui/fragments/homescreen/HomeScreenAdapter.kt
+++ app/src/main/java/edu/neu/madcourse/trexercize/ui/fragments/homescreen/HomeScreenAdapter.kt
@@ -1,8 +1,10 @@
 package edu.neu.madcourse.trexercize.ui.fragments.homescreen
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import edu.neu.madcourse.trexercize.R
 import edu.neu.madcourse.trexercize.ui.fragments.exercise.EachExerciseCardListener
@@ -21,7 +23,10 @@ class HomeScreenAdapter(
     override fun onBindViewHolder(holder: HomeScreenViewHolder, position: Int) {
         holder.favoriteExercise.text = favoritesList[position].favoriteExercise.toString()
         holder.exerciseCategory.text = favoritesList[position].exerciseCategory.toString()
-        favoritesList[position].categoryImage?.let { holder.categoryImage.setImageResource(it) }
+        favoritesList[position].categoryImage?.let { holder.categoryImage.setImageResource(it)
+            if (position % 2 == 0) {
+                holder.background.setBackgroundColor(Color.rgb(92,188,179))
+            }}
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/edu/neu/madcourse/trexercize/ui/fragments/homescreen/HomeScreenViewHolder.kt
+++ app/src/main/java/edu/neu/madcourse/trexercize/ui/fragments/homescreen/HomeScreenViewHolder.kt
@@ -3,7 +3,7 @@ package edu.neu.madcourse.trexercize.ui.fragments.homescreen
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import edu.neu.madcourse.trexercize.R
 import edu.neu.madcourse.trexercize.ui.fragments.exercise.EachExerciseCardListener
@@ -12,6 +12,7 @@ class HomeScreenViewHolder(itemView: View, listener: EachExerciseCardListener) :
     var favoriteExercise: TextView = itemView.findViewById(R.id.favorite_exercise_name)
     var exerciseCategory: TextView = itemView.findViewById(R.id.favorite_exercise_category)
     var categoryImage: ImageView = itemView.findViewById(R.id.favorite_category_image)
+    var background: CardView = itemView.findViewById(R.id.card_view)
 
     init {
         itemView.setOnClickListener {
